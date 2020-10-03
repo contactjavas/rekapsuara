b17ba70ef220a33b67093de76edc9817f9c99918
Bagus <25834188+contactjavas@users.noreply.github.com>
2020-10-03 13:40:16 +0700

    change api domain

--- src/app/services/app/app.service.ts
+++ src/app/services/app/app.service.ts
@@ -5,8 +5,8 @@ import { HttpHeaders } from "@angular/common/http";
   providedIn: "root"
 })
 export class AppService {
-  public apiUrl = "https://rekapsuara.divren.co.id/wp-json/rekap-suara/v1";
-  public jwtAuthUrl = "https://rekapsuara.divren.co.id/wp-json/jwt-auth/v1";
+  public apiUrl = "https://votty-qc.diggy.id/wp-json/rekap-suara/v1";
+  public jwtAuthUrl = "https://votty-qc.diggy.id/wp-json/jwt-auth/v1";
 
   public httpOptions = {
     headers: new HttpHeaders({
